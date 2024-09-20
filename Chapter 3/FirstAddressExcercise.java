8aa87a2916855aa9020b655c3414ccd8fb47f333
Bridget Narte <149249862+bridget-narte@users.noreply.github.com>
2024-09-20 22:19:33 +0800

    Added FirstAddressExercise.java

--- Chapter 3/FirstAddressExcercise.java
+++ Chapter 3/FirstAddressExcercise.java
@@ -6,10 +6,10 @@ public class FirstAddressExcercise
 	}
 	
 	public static void displayAddress() {
-        System.out.println("Google Pixel Company");
-        System.out.println("1600 Amphitheatre Parkway, California, U.S.");
-        System.out.println("Mountain View");
-        System.out.println("Company: Alphabet Inc.");
+        	System.out.println("Google Pixel Company");
+        	System.out.println("1600 Amphitheatre Parkway, California, U.S.");
+        	System.out.println("Mountain View");
+        	System.out.println("Company: Alphabet Inc.");
 
 	} 
 }
