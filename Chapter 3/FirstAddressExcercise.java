246bdd10db0c5c7429ea136dcbf24536d860ec24
Bridget Narte <149249862+bridget-narte@users.noreply.github.com>
2024-09-20 22:12:09 +0800

    Added FirstAddressExcercise.java to Chapter 3

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,15 @@
+public class FirstAddressExcercise
+{
+	public static void main(String[] args) {
+		System.out.println("First Java application");
+		displayAddress();
+	}
+	
+	public static void displayAddress() {
+        System.out.println("Google Pixel Company");
+        System.out.println("1600 Amphitheatre Parkway, California, U.S.");
+        System.out.println("Mountain View");
+        System.out.println("Company: Alphabet Inc.");
+
+	} 
+}
