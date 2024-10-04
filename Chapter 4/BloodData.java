23717daedef689d43260c8a8fc07996b37ee11e8
Bridget Narte <149249862+bridget-narte@users.noreply.github.com>
2024-10-04 22:54:15 +0800

    Add TestBloodData.java

---
+++ Chapter 4/BloodData.java
@@ -0,0 +1,31 @@
+public class BloodData {
+
+    static String bloodType, rhFactor;
+
+    public static void defaultPatient() {
+        bloodType = "A";
+        rhFactor = "-";
+    }
+
+    public static void setBloodtype(String newBloodType) {
+        bloodType = newBloodType;
+    }
+
+    public static void setRHFACTOR(String newRh) {
+        rhFactor = newRh;
+    }
+
+    public static String getBlood() {
+        return bloodType;
+    }
+
+    public static String getRHFACTOR() {
+        return rhFactor;
+    }
+
+    public static void displayBloodInfo() {
+        System.out.println("Your blood type is : " + getBlood());
+        System.out.println("Your Rh Factor is: " + getRHFACTOR());
+
+    }
+}
---
+++ Chapter 4/TestBloodData.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class TestBloodData {
+    public static void main(String[] args) {
+        BloodData.defaultPatient();
+        System.out.println("Default Blood Type:");
+
+        BloodData.displayBloodInfo();
+        System.out.println();
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter blood type: ");
+        String bloodType = scan.nextLine();
+        System.out.println("Enter RH Factor: ");
+        String rhFactor = scan.nextLine();
+
+
+        BloodData.setBloodtype(bloodType);
+        BloodData.setRHFACTOR(rhFactor);
+        BloodData.displayBloodInfo();
+    }
+}
